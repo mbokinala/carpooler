24c0814e1457e2d74bf9dbc726ec449b59e54c2c
mbokinala <manav.bokinala@gmail.com>
2021-06-12 15:17:32 -0700

    added basic google maps api

--- pages/AddCarpool.tsx
+++ pages/AddCarpool.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+import Head from 'next/head';
+import React from 'react';
 
 function AddCarpool() {
+
 	return (
-		<div>
-			
-		</div>
+		<>
+			<Head>
+				<title>Add Carpool</title>
+				<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBLr5qel5Lbjw7IfPaIX2ORcn4Qn_7_3mA&libraries=places" />
+				{/* <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?libraries=places‌​&sensor=false"/> */}
+				<script src="/google-maps-api.js"/>
+			</Head>
+
+			<input style={{width: "100%"}} id="ship-address"></input><br></br>
+			<input id="address2"></input>
+			<input id="postcode"></input>
+			<input id="locality"></input>
+			<input id="state"></input>
+			<input id="country"></input>
+		</>
 	)
 }
 
-export default AddCarpool
+export default withPageAuthRequired(AddCarpool);
