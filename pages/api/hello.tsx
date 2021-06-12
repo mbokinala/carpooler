eb0635787e0cdcf09de3c69768fbbbfe999ffc98
mbokinala <manav.bokinala@gmail.com>
2021-06-11 23:56:42 -0700

    removed testing route

--- pages/api/hello.tsx
+++
@@ -1,7 +0,0 @@
-import query from "../../db";
-
-export default async function handler(req, res) {
-    let response = await query("DESCRIBE users");
-    console.log(response);
-    return res.status(200).send(response);
-}
