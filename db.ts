4a66ea3bff3e3093165d26b93226a6d7b347a0bc
mbokinala <manav.bokinala@gmail.com>
2021-06-12 15:22:36 -0700

    converted db to custom promise

--- db.ts
+++ db.ts
@@ -8,5 +8,12 @@ const pool = mysql.createPool({
 	database: 'carpooler'
 })
 
-const query = util.promisify(pool.query).bind(pool); // convert callback to promise
+const query = (queryString) => {
+	return new Promise((resolve, reject) => {
+		pool.query(queryString, function(error, results, fields) {
+			if (error) return reject(error);
+			resolve({results, fields});
+		})
+	});
+}
 export default query;
