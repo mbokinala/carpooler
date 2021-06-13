15bd533c13d7953d5f58df829185c4073a9db434
mbokinala <manav.bokinala@gmail.com>
2021-06-13 00:43:22 -0700

    added a lot

--- pages/api/carpool/search.ts
+++ pages/api/carpool/search.ts
@@ -2,17 +2,18 @@ import query from "../../../db";
 
 export default async function handler(req, res) {
     let results = [];
-    const target_location = (await query(`SELECT * FROM target_locations WHERE name LIKE "%${req.body.search}%"`)).results[0];
+    const target_location = (await query(`SELECT * FROM target_locations WHERE name LIKE "%${req.query.q}%"`)).results[0];
 
     const matching_carpools = (await query(`SELECT * FROM carpools WHERE target_location_id = "${target_location.target_location_id}"`)).results;
 
     for (const carpool of matching_carpools) {
-        const start_location = (await query(`SELECT * FROM starting_locations WHERE carpool_id = "${carpool.carpool_id}"`)).results[0];
-        const timings = (await query(`SELECT * FROM timings WHERE carpool_id = ${carpool.carpool_id}`)).results[0];
+        const start_location = (await query(`SELECT * FROM starting_locations WHERE starting_location_id = "${carpool.starting_location_id}"`)).results[0];
+        const timings = (await query(`SELECT * FROM timings WHERE carpool_id = "${carpool.carpool_id}"`)).results[0];
 
         results.push({
+            id: carpool.carpool_id,
             targetLocation: target_location,
-            start_location: start_location,
+            startingLocation: start_location,
             timings: timings
         });
     }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,54 @@
+import { ReactNode } from 'react';
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  Link,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+
+// const Links = ['Profile', 'Projects', 'Team'];
+
+// const NavLink = ({ children }: { children: ReactNode }) => (
+//   <Link
+//     px={2}
+//     py={1}
+//     rounded={'md'}
+//     _hover={{
+//       textDecoration: 'none',
+//       bg: useColorModeValue('gray.200', 'gray.700'),
+//     }}
+//     href={'#'}>
+//     {children}
+//   </Link>
+// );
+
 export default function Home() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <>
-      <p>This is carpooler</p>
-      <a href="/profile">Profile</a> <br></br>
-      <a href="/api/auth/login">Login</a> <br></br>
-      <a href="/api/auth/logout">Logout</a>
-    </>
-  )
+    <Stack direction="column" spacing={4} align="center" mt="50%">
+      <Button colorScheme="teal" variant="solid">
+        Find Carpools
+      </Button>
+      <Button colorScheme="teal" variant="solid">
+        Add Carpool
+      </Button>
+      <Button colorScheme="teal" variant="solid" width="20vh">
+        Profile
+      </Button>
+     
+    </Stack>
+  );
 }
